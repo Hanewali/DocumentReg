e93f5053407258c8b595179ddc60fc5e3fe61bcc
Jakub Rumpel <jakub@life-coder.pl>
2021-05-23 18:21:34 +0200

    Added IModel interface for base of domain models and User model class

---
+++ DocumentRegistry/DocumentRegistry/DomainModels/IModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DocumentRegistry.DomainModels
+{
+    public interface IModel
+    {
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public User CreateUser { get; set; }
+        public DateTime ModifyDate { get; set; }
+        public User ModifyUser { get; set; }
+    }
+}
---
+++ DocumentRegistry/DocumentRegistry/DomainModels/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Dapper.Contrib.Extensions;
+
+namespace DocumentRegistry.DomainModels
+{
+    [Table("User")]
+    public class User : IModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public User CreateUser { get; set; }
+        public DateTime ModifyDate { get; set; }
+        public User ModifyUser { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string PasswordSalt { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
