a50239b07ea925551d6b01f71a7a772202227785
Jakub Rumpel <jakub@life-coder.pl>
2021-05-23 23:19:33 +0200

    Added custom configuration setup in Startup

--- DocumentRegistry/DocumentRegistry/Startup.cs
+++ DocumentRegistry/DocumentRegistry/Startup.cs
@@ -1,27 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using DocumentRegistry.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace DocumentRegistry
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup()
         {
-            Configuration = configuration;
+            Configuration.SetConfiguration();
         }
 
-        public IConfiguration Configuration { get; }
+        public Configuration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
