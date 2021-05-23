e43c74acac6199cbc436b955a5a3853ec27dd0ba
Jakub Rumpel <jakub@life-coder.pl>
2021-05-23 19:50:04 +0200

    Added domain model for company table

---
+++ DocumentRegistry/DocumentRegistry/DomainModels/Company.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using Dapper.Contrib.Extensions;
+
+namespace DocumentRegistry.DomainModels
+{
+    [Table("Company")]
+    public class Company : IModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public User CreateUser { get; set; }
+        public DateTime ModifyDate { get; set; }
+        public User ModifyUser { get; set; }
+        public string Name { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Branch { get; set; }
+        public string PostalCode { get; set; }
+        public string PostName { get; set; }
+        public string PostStreet { get; set; }
+        public string PostCity { get; set; }
+        public string PostPostalCode { get; set; }
+    }
+}
