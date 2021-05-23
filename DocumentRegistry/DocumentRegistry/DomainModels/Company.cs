5e1a8ba1060ed02af92d598c9be5b6cb90692c9b
Jakub Rumpel <jakub@life-coder.pl>
2021-05-23 23:16:52 +0200

    Changed Company class to ModelBase

--- DocumentRegistry/DocumentRegistry/DomainModels/Company.cs
+++ DocumentRegistry/DocumentRegistry/DomainModels/Company.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Reflection;
-using Dapper.Contrib.Extensions;
+﻿using Dapper.Contrib.Extensions;
 
 namespace DocumentRegistry.DomainModels
 {
     [Table("Company")]
-    public class Company : IModel
+    public class Company : ModelBase
     {
-        [Key]
-        public int Id { get; set; }
-        public DateTime CreateDate { get; set; }
-        public User CreateUser { get; set; }
-        public DateTime ModifyDate { get; set; }
-        public User ModifyUser { get; set; }
         public string Name { get; set; }
         public string Street { get; set; }
         public string City { get; set; }
