bf6bc5a2f0c1854fcdd1b0207fc59d2611cd17cd
Jakub Rumpel <jakub@life-coder.pl>
2021-05-23 19:21:40 +0200

    Added basic configuration model with connection string and database settings

---
+++ DocumentRegistry/DocumentRegistry/Infrastructure/Configuration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Transactions;
+using Microsoft.Extensions.Configuration;
+
+namespace DocumentRegistry.Infrastructure
+{
+    public class Configuration
+    {
+        private static IConfigurationRoot _configuration;
+
+        public static class Database
+        {
+            public static string ConnectionString = _configuration.GetConnectionString("ConnectionString");
+            public static IsolationLevel DefaultIsolationLevel = IsolationLevel.ReadUncommitted;
+            public static TimeSpan DefaultTimeout = TransactionManager.DefaultTimeout;
+        }
+
+        public static string GetConfigurationField(string fieldName)
+        {
+            return _configuration[fieldName];
+        }
+
+        public static void SetConfiguration()
+        {
+            _configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+        }
+    }
+}
