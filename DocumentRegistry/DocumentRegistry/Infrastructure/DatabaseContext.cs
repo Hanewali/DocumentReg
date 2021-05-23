4d7ec28cd9cb055d388806c99a07f7e136dea8cc
Jakub Rumpel <jakub@life-coder.pl>
2021-05-23 19:22:17 +0200

    Added database context to be used with database helper

---
+++ DocumentRegistry/DocumentRegistry/Infrastructure/DatabaseContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using Dapper;
+using IsolationLevel = System.Transactions.IsolationLevel;
+
+
+namespace DocumentRegistry.Infrastructure
+{
+    public class DatabaseContext
+    {
+        public string SqlCommand { get; private set; }
+        public DynamicParameters Parameters { get; private set; }
+        public CommandType CommandType { get; private set; }
+        public IsolationLevel IsolationLevel { get; private set; }
+        public TimeSpan Timeout { get; set; }
+
+        public DatabaseContext(string sqlCommand, TimeSpan timeout, IsolationLevel isolationLevel, CommandType commandType = CommandType.Text, DynamicParameters parameters = null)
+        {
+            SqlCommand = sqlCommand;
+            CommandType = commandType;
+            Parameters = parameters;
+            IsolationLevel = isolationLevel;
+            Timeout = timeout;
+        }
+        public DatabaseContext(string sqlCommand, IsolationLevel isolationLevel, CommandType commandType = CommandType.Text, DynamicParameters parameters = null)
+            : this(sqlCommand, Configuration.Database.DefaultTimeout, isolationLevel, commandType, parameters)
+        {
+        }
+
+        public DatabaseContext(string sqlCommand, CommandType commandType = CommandType.Text, DynamicParameters parameters = null)
+            : this(sqlCommand, Configuration.Database.DefaultIsolationLevel, commandType, parameters)
+        {
+        }
+    }
+}
