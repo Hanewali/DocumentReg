d28f94ad82ae828ab8ee53745199dc1923130c13
Jakub Rumpel <jakub@life-coder.pl>
2021-05-25 17:39:28 +0200

    Added CompanyController.cs to web app project

---
+++ DocumentRegistry/DocumentRegistry.Web/Controllers/CompanyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using DocumentRegistry.Web.Services.CompanyService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocumentRegistry.Web.Controllers
+{
+    [Route("[controller]")]
+    public class CompanyController : Controller
+    {
+        private static ICompanyService _companyService;
+
+        public CompanyController(ICompanyService companyService)
+        {
+            _companyService = companyService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var model = _companyService.PrepareIndexModel();
+            return View(model);
+        }
+    }
+}
