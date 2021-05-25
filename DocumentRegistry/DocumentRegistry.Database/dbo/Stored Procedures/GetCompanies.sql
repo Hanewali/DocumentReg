40920437ff292e6e2869fffaa884c6be2727dbdd
Jakub Rumpel <jakub@life-coder.pl>
2021-05-25 17:54:24 +0200

    Added GetCompanies procedure to database project

---
+++ DocumentRegistry/DocumentRegistry.Database/dbo/Stored Procedures/GetCompanies.sql
@@ -0,0 +1,28 @@
+﻿USE [DocumentRegistry]
+GO
+
+/****** Object:  StoredProcedure [dbo].[GetCompanies]    Script Date: 25.05.2021 17:53:38 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		Jakub Rumpel
+-- Create date: 2021-05-23
+-- Description:	Pobiera listę firm z bazy - to jest wstępna wersja procedury, żeby zweryfikać całą komunikację WebApp-WebApi-DB 
+-- =============================================
+CREATE PROCEDURE [dbo].[GetCompanies]
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT
+        *
+    from
+        Company
+END
+GO
+
+
