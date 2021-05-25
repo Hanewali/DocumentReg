dfa4d28ba3ef46a1183bbc13ce36df3f675ffc90
Jakub Rumpel <jakub@life-coder.pl>
2021-05-25 17:40:28 +0200

    Added early models for views and layouts

---
+++ DocumentRegistry/DocumentRegistry.Web/Models/Company/Company.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace DocumentRegistry.Web.Models.Company
+{
+    public class Company
+    {
+        [DisplayName("Nazwa")]
+        public string Name { get; set; }
+        [DisplayName("Ulica")]
+        public string Street { get; set; }
+        [DisplayName("Miasto")]
+        public string City { get; set; }
+        [DisplayName("Oddział")]
+        public string Branch { get; set; }
+        [DisplayName("Kod pocztowy")]
+        public string PostalCode { get; set; }
+        [DisplayName("Nazwa")]
+        public string PostName { get; set; }
+        [DisplayName("Ulica")]
+        public string PostStreet { get; set; }
+        [DisplayName("Miasto")]        
+        public string PostCity { get; set; }
+        [DisplayName("Kod pocztowy")]
+        public string PostPostalCode { get; set; }
+    }
+}
---
+++ DocumentRegistry/DocumentRegistry.Web/Models/Company/Index.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace DocumentRegistry.Web.Models.Company
+{
+    public class Index
+    {
+        public Index(IEnumerable<Company> companies)
+        {
+            Companies = companies;
+        }
+
+        public IEnumerable<Company> Companies { get; set; }
+    }
+}
