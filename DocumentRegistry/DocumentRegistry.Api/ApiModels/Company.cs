5a811654934c70311ed49b3815d41ad1ce1bc6d6
Jakub Rumpel <jakub@life-coder.pl>
2021-05-25 22:31:21 +0200

    Added company ApiModel to be returned to WebApp

---
+++ DocumentRegistry/DocumentRegistry.Api/ApiModels/Company.cs
@@ -0,0 +1,31 @@
+﻿namespace DocumentRegistry.Api.ApiModels
+{
+    public class Company
+    {
+        public string Name { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Branch { get; set; }
+        public string PostalCode { get; set; }
+        public string PostName { get; set; }
+        public string PostStreet { get; set; }
+        public string PostCity { get; set; }
+        public string PostPostalCode { get; set; }
+
+        public static Company BuildFromDomainModel(DomainModels.Company company)
+        {
+            return new()
+            {
+                Name = company.Name,
+                Street = company.Street,
+                City = company.City,
+                Branch = company.Branch,
+                PostalCode = company.PostalCode,
+                PostName = company.PostName,
+                PostStreet = company.PostStreet,
+                PostCity = company.PostCity,
+                PostPostalCode = company.PostPostalCode
+            };
+        }
+    }
+}
