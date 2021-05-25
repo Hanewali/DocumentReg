7f98c43bd6e3253014fd02cb7a889c0118083698
Jakub Rumpel <jakub@life-coder.pl>
2021-05-26 00:39:02 +0200

    Added Login request and response model

---
+++ DocumentRegistry/DocumentRegistry.Api/ApiModels/Login/LoginRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Linq;
+using DocumentRegistry.Web.Models;
+
+namespace DocumentRegistry.Api.ApiModels.Login
+{
+    public class LoginRequest : RequestModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ DocumentRegistry/DocumentRegistry.Api/ApiModels/Login/LoginResponse.cs
@@ -0,0 +1,11 @@
+﻿
+using DocumentRegistry.Web.Models;
+
+namespace DocumentRegistry.Api.ApiModels.Login
+{
+    public class LoginResponse : ResponseModel
+    {
+        public bool Verified { get; set; }
+        public int UserId { get; set; }
+    }
+}
