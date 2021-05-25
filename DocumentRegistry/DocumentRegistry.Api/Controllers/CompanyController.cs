154b60e94fa0fac28a940f60e0dc34d88aadddef
Jakub Rumpel <jakub@life-coder.pl>
2021-05-25 23:00:38 +0200

    Corrected local DB Password

--- DocumentRegistry/DocumentRegistry.Api/Controllers/CompanyController.cs
+++ DocumentRegistry/DocumentRegistry.Api/Controllers/CompanyController.cs
@@ -1,22 +1,40 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text.Json;
 using DocumentRegistry.Api.DomainModels;
 using DocumentRegistry.Api.Helpers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace DocumentRegistry.Api.Controllers
 {
     [ApiController, Route("[controller]")]   
     public class CompanyController : Extensions.Controller
     {
+        private readonly ILogger<CompanyController> _logger;
+
+        public CompanyController(ILogger<CompanyController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet]
         public IActionResult GetList()
         {
             var result = new List<ApiModels.Company>();
-            
-            var queryResult = DatabaseHelper.ExecProcedure<Company>("GetCompanies");
-            result.AddRange(queryResult.Select(ApiModels.Company.BuildFromDomainModel));
+
+            try
+            {
+                var queryResult = DatabaseHelper.ExecProcedure<Company>("GetCompanies");
+                result.AddRange(queryResult.Select(ApiModels.Company.BuildFromDomainModel));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error during CompanyController.GetList", ex);
+                return new StatusCodeResult((int) HttpStatusCode.InternalServerError);
+            }
       
             return Ok(JsonSerializer.Serialize(result));
         }
