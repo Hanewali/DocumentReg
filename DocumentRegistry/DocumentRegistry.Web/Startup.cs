c3ad44aed2f1a89ac0a4e1f3d8c9e1ba14c929bd
Jakub Rumpel <jakub@life-coder.pl>
2021-05-25 17:42:07 +0200

    Added custom configuration loading using special Configuration.cs class

--- DocumentRegistry/DocumentRegistry.Web/Startup.cs
+++ DocumentRegistry/DocumentRegistry.Web/Startup.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using DocumentRegistry.Web.Infrastructure;
+using DocumentRegistry.Web.Services.CompanyService;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -13,17 +9,18 @@ namespace DocumentRegistry.Web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup()
         {
-            Configuration = configuration;
+            Configuration.SetConfiguration();
         }
 
-        public IConfiguration Configuration { get; }
+        public Configuration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddScoped<ICompanyService, CompanyService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
