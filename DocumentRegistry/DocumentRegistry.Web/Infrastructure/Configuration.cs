345bb62eb95253aca86601e220499732759cd55b
Jakub Rumpel <jakub@life-coder.pl>
2021-05-25 17:39:56 +0200

    Added custom configuration class with API fields

---
+++ DocumentRegistry/DocumentRegistry.Web/Infrastructure/Configuration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Transactions;
+using Microsoft.Extensions.Configuration;
+
+namespace DocumentRegistry.Web.Infrastructure
+{
+    public class Configuration
+    {
+        private static IConfigurationRoot _configuration;
+
+        public static class Api
+        {
+            public static string AuthorizationToken = GetConfigurationField("Api:AuthorizationToken");
+            public static string Url = GetConfigurationField("Api:Url");
+        }
+        
+        public static string GetConfigurationField(string fieldName)
+        {
+            return _configuration[fieldName];
+        }
+
+        public static void SetConfiguration()
+        {
+            _configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.Development.json", optional: false, reloadOnChange: true) //todo:Can I get right appsettings file based on environment?
+                .Build();
+            
+            
+        }
+    }
+}
