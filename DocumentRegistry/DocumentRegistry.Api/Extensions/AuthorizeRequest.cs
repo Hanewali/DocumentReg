68681816aacf0ead966acd3660e25e52d784627b
Jakub Rumpel <jakub@life-coder.pl>
2021-05-25 17:36:36 +0200

    Added extension methods for validating request header against API Authorization Token

---
+++ DocumentRegistry/DocumentRegistry.Api/Extensions/AuthorizeRequest.cs
@@ -0,0 +1,13 @@
+﻿
+using DocumentRegistry.Api.Infrastructure;
+
+namespace DocumentRegistry.Api.Extensions
+{
+    public static class HttpRequest
+    {
+        public static bool IsAuthorized(this Microsoft.AspNetCore.Http.HttpRequest request)
+        {
+            return request.Headers["AuthorizationToken"] == Configuration.Api.AuthorizationToken;
+        }
+    }
+}
