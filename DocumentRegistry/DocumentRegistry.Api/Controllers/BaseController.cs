0e0662ae98305733340dcdb8f040dda5d7feebc2
Jakub Rumpel <jakub@life-coder.pl>
2021-05-25 23:28:14 +0200

    Moved Controller.cs extension to BaseController in Controllers

---
+++ DocumentRegistry/DocumentRegistry.Api/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using DocumentRegistry.Api.Exceptions;
+using DocumentRegistry.Api.Extensions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Serilog;
+
+namespace DocumentRegistry.Api.Controllers
+{
+    public class BaseController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (Request.IsAuthorized()) return;
+            
+            Log.Error("Wrong authorization token!");
+            throw new NotAuthorizedException("Wrong authorization token");
+        }
+    }
+}
