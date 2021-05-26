d213b7a7dafb12ce4b8d53cc2878ba2a60ac4c4d
Jakub Rumpel <jakub@life-coder.pl>
2021-05-26 13:27:19 +0200

    Added logger to CompanyController.cs

--- DocumentRegistry/DocumentRegistry.Web/Controllers/CompanyController.cs
+++ DocumentRegistry/DocumentRegistry.Web/Controllers/CompanyController.cs
@@ -1,17 +1,20 @@
 ﻿using System.Net;
 using DocumentRegistry.Web.Services.CompanyService;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace DocumentRegistry.Web.Controllers
 {
     [Route("[controller]")]
     public class CompanyController : Controller
     {
+        private ILogger<CompanyController> _logger;
         private static ICompanyService _companyService;
 
-        public CompanyController(ICompanyService companyService)
+        public CompanyController(ICompanyService companyService, ILogger<CompanyController> logger)
         {
             _companyService = companyService;
+            _logger = logger;
         }
 
         [HttpGet]
