0a3633811c0fe494f539a82a83f3aea0b39211da
Jakub Rumpel <jakub@life-coder.pl>
2021-05-26 13:30:22 +0200

    Added basic Login response and request models

---
+++ DocumentRegistry/DocumentRegistry.Web/Models/Home/Login.cs
@@ -0,0 +1,8 @@
+﻿namespace DocumentRegistry.Web.Models.Home
+{
+    public class Login : RequestModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ DocumentRegistry/DocumentRegistry.Web/Models/Home/LoginResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace DocumentRegistry.Web.Models.Home
+{
+    public class LoginResponse : ResponseModel
+    {
+        public bool Verified { get; set; }
+        public int UserId { get; set; }
+    }
+}
