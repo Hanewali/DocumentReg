7bbea3e36dbf12454b3b0c235f2c5c267b2ea62a
Jakub Rumpel <jakub@life-coder.pl>
2021-05-26 13:27:52 +0200

    Added basic LoginService with Verify and Login methods

---
+++ DocumentRegistry/DocumentRegistry.Web/Services/HomeService/ILoginService.cs
@@ -0,0 +1,10 @@
+﻿using DocumentRegistry.Web.Models.Home;
+
+namespace DocumentRegistry.Web.Services.HomeService
+{
+    public interface ILoginService
+    {
+        LoginResponse Verify(Login request);
+        void Login(LoginResponse request);
+    }
+}
---
+++ DocumentRegistry/DocumentRegistry.Web/Services/HomeService/LoginService.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Net.Mime;
+using System.Text;
+using System.Text.Json;
+using DocumentRegistry.Web.Models.Home;
+
+namespace DocumentRegistry.Web.Services.HomeService
+{
+    public class LoginService : ILoginService
+    {
+        private HttpClient _client;
+        
+        public LoginService()
+        {
+            _client = ApiHelper.PrepareClient();
+        }
+
+        public LoginResponse Verify(Login request)
+        {
+            var response = _client
+                .PostAsync("Login/Verify", new StringContent(request.ToJson(), Encoding.UTF8, "application/json"))
+                .Result.Content.ReadAsStringAsync().Result;
+            
+             return LoginResponse.FromJson<LoginResponse>(response);
+        }
+
+        public void Login(LoginResponse request)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
