7d5d40e80dc20d152307cdeaabd8ec7fbd2c210c
Jakub Rumpel <jakub@life-coder.pl>
2021-05-26 13:27:35 +0200

    Created ApiHelper with PrepareClient method

---
+++ DocumentRegistry/DocumentRegistry.Web/Services/ApiHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using DocumentRegistry.Web.Infrastructure;
+
+namespace DocumentRegistry.Web.Services
+{
+    public static class ApiHelper
+    {
+        public static HttpClient PrepareClient()
+        {
+            return new()
+            {
+                BaseAddress = new Uri(Configuration.Api.Url)
+            };
+        }
+    }
+}
