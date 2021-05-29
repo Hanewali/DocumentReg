72d69ad6a0d2912a7dc4ac5391e536e1d1391019
Jakub Rumpel <jakub@life-coder.pl>
2021-05-29 19:43:54 +0200

    Added PostCompany requestmodel

---
+++ DocumentRegistry/DocumentRegistry.Web/ApiModels/PostCompanyRequest.cs
@@ -0,0 +1,12 @@
+﻿using DocumentRegistry.Web.Models.PostCompany;
+
+namespace DocumentRegistry.Web.ApiModels
+{
+    public class PostCompanyRequest
+    {
+        public int UserId { get; set; }
+        public int BeginFrom { get; set; }
+        public int Rows { get; set; }
+        public PostCompany PostCompany { get; set; }
+    }
+}
