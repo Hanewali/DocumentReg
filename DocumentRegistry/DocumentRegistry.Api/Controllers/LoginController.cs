396568d29fe2b1537dd888cf4fb2462d2eaa7876
Jakub Rumpel <jakub@life-coder.pl>
2021-05-29 11:27:02 +0200

    Added handling passwords during login

--- DocumentRegistry/DocumentRegistry.Api/Controllers/LoginController.cs
+++ DocumentRegistry/DocumentRegistry.Api/Controllers/LoginController.cs
@@ -1,29 +1,33 @@
 ﻿using System;
 using System.Security.Cryptography;
+using System.Text.Json;
 using Dapper;
 using DocumentRegistry.Api.ApiModels.Login;
 using DocumentRegistry.Api.DomainModels;
 using DocumentRegistry.Api.Helpers;
-using DocumentRegistry.Api.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace DocumentRegistry.Api.Controllers
 {
-    [ApiController, Route("[controller]/[action]")]
-    public class LoginController : Controller
+    [ApiController, Route("[controller]")]
+    public class LoginController : BaseController
     {
         [HttpPost]
-        public IActionResult VerifyLogin([FromBody] LoginRequest request)
+        public ActionResult VerifyLogin(LoginRequest request)
         {
+            if (!ModelState.IsValid)
+                return Problem();
+            
             var parameters = new DynamicParameters();
             parameters.Add("Username", request.Username);
             parameters.Add("Password", request.Password);
 
-            var user = DatabaseHelper.GetValue<User>($"select * from User where Login = {request.Username}");
+            var user = DatabaseHelper.QueryFirst<User>($"select * from [User] where Login = '{request.Username}'");
 
             if (VerifyPassword(user.PasswordHash, user.PasswordSalt, request.Password))
             {
-                return Ok(LoginResponse.BuildFromUser(user).ToJson());
+                return Ok(JsonSerializer.Serialize(LoginResponse.BuildFromUser(user)));
             }
 
             return Forbid();
@@ -31,13 +35,12 @@ namespace DocumentRegistry.Api.Controllers
 
         private bool VerifyPassword(string passwordHash, string passwordSalt, string requestPlainPassword)
         {
-            var passwordHashBytes = Convert.FromBase64String(passwordHash);
-            var passwordSaltBytes = Convert.FromBase64String(passwordSalt);
-            var pbkdf2 = new Rfc2898DeriveBytes(requestPlainPassword, passwordSaltBytes, 100000);
+            var hashBytes = Convert.FromBase64String(passwordHash);
+            var salt = Convert.FromBase64String(passwordSalt);
+            var pbkdf2 = new Rfc2898DeriveBytes(requestPlainPassword, salt, 100000);
             var hash = pbkdf2.GetBytes(20);
-            /* Compare the results */
-            for (var i=0; i < 10; i++)
-                if (passwordHashBytes[i] != hash[i])
+            for (int i=0; i < 20; i++)
+                if (hashBytes[i] != hash[i])
                     return false;
 
             return true;
