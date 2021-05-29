9b658a26ec11e86feb48c568d820002c9dfab3b5
Jakub Rumpel <jakub@life-coder.pl>
2021-05-29 22:25:17 +0200

    Added search and create actions to company controller

--- DocumentRegistry/DocumentRegistry.Web/Controllers/CompanyController.cs
+++ DocumentRegistry/DocumentRegistry.Web/Controllers/CompanyController.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using DocumentRegistry.Web.ApiModels;
+using DocumentRegistry.Web.Models;
+using DocumentRegistry.Web.Models.Company;
 using DocumentRegistry.Web.Services.CompanyService;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -17,23 +23,57 @@ namespace DocumentRegistry.Web.Controllers
             _logger = logger;
         }
 
-        [HttpGet]
-        public IActionResult Index()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Search(CompanyRequest model)
         {
-            var model = new Models.Company.Index();
+            var searchResult = new List<Company>();
+            
             try
             {
-                 model = _companyService.PrepareIndexModel();
+                searchResult.AddRange(_companyService.Search(model.Company, model.BeginFrom, model.Rows, HttpContext.Session.GetInt32("UserId").Value));
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex,"Error during API request");
-                Response.StatusCode = 500;
-                
-                return View();
+                _logger.LogError(ex, "There was an error during company search");
+                return Problem();
             }
-            
-            return View(model);
+
+            return Ok(JsonSerializer.Serialize(searchResult));
+        }
+
+        [HttpGet]
+        public IActionResult Details(int Id)
+        {
+            var result = new Company();
+
+            try
+            {
+                result = _companyService.GetDetails(Id, HttpContext.Session.GetInt32("UserId").Value);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "There was an error during company search");
+                return Problem();
+            }
+
+            return View(result);
+        }
+        
+        [HttpPost]
+        public IActionResult Create(Company company)
+        {
+            try
+            {
+                _companyService.Create(company, HttpContext.Session.GetInt32("UserId").Value);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "There was an error during company search");
+                return Problem();
+            }
+
+            return RedirectToAction("Search", "Company");
         }
     }
 }
