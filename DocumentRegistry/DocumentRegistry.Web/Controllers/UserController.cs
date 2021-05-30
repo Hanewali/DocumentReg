a7d438284f34b53aad481bd0b0aa6e025be588dd
Jakub Rumpel <jakub@life-coder.pl>
2021-05-30 14:40:22 +0200

    Added basic actions to UserController.cs

--- DocumentRegistry/DocumentRegistry.Web/Controllers/UserController.cs
+++ DocumentRegistry/DocumentRegistry.Web/Controllers/UserController.cs
@@ -1,13 +1,132 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using DocumentRegistry.Web.ApiModels;
+using DocumentRegistry.Web.Models.User;
+using DocumentRegistry.Web.Services.UserService;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace DocumentRegistry.Web.Controllers
 {
-    public class UserController : Controller
+    public class UserController : BaseController
     {
-        // GET
-        public IActionResult Index()
+        private readonly ILogger<UserController> _logger;
+        private static IUserService _companyService;
+
+        public UserController(IUserService companyService, ILogger<UserController> logger)
         {
-            return View();
+            _companyService = companyService;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Search(UserRequest model)
+        {
+            var searchResult = new List<User>();
+            
+            try
+            {
+                searchResult.AddRange(_companyService.Search(model.User, model.BeginFrom, model.Rows, GetUserIdFromSession()));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "There was an error during company search");
+                return Problem();
+            }
+
+            return Ok(JsonSerializer.Serialize(searchResult));
+        }
+
+        [HttpGet]
+        public IActionResult Details(int companyId)
+        {
+            var result = new User();
+
+            try
+            {
+                result = _companyService.GetDetails(companyId, GetUserIdFromSession());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "There was an error during company search");
+                return Problem();
+            }
+
+            return View(result);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var model = new User();
+
+            return View(model);
+        }
+        
+        [HttpPost]
+        public IActionResult Create(User company)
+        {
+            try
+            {
+                _companyService.Create(company, GetUserIdFromSession());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "There was an error during company search");
+                return Problem();
+            }
+
+            return RedirectToAction("Search", "User");
+        }
+
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            var model = new User();
+
+            return View(model);
+        }
+        
+        [HttpPost]
+        public IActionResult Edit(User company)
+        {
+            try
+            {
+                _companyService.Edit(company, GetUserIdFromSession());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "There was an error during company search");
+                return Problem();
+            }
+
+            return RedirectToAction("Details", "User", company.Id);
+        }
+        
+        [HttpGet]
+        public IActionResult Delete()
+        {
+            var model = new User();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int companyId)
+        {
+            try
+            {
+                _companyService.Delete(companyId, GetUserIdFromSession());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "There was an error during company search");
+                return Problem();
+            }
+
+            return RedirectToAction("Search", "User");
         }
     }
 }
