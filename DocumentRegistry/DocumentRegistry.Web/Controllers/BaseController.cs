51e5b5640799e0ac34f041bbc635978ba9971e00
Jakub Rumpel <jakub@life-coder.pl>
2021-05-30 18:35:30 +0200

    Added OnActionExecuting login verification in BaseController

--- DocumentRegistry/DocumentRegistry.Web/Controllers/BaseController.cs
+++ DocumentRegistry/DocumentRegistry.Web/Controllers/BaseController.cs
@@ -1,10 +1,22 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace DocumentRegistry.Web.Controllers
 {
     public class BaseController : Controller
     {
         protected int GetUserIdFromSession() => HttpContext.Session.GetInt32("UserId").Value;
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (HttpContext.Session.GetInt32("UserId") == null)
+            {
+                context.Result = new RedirectResult("/");
+                return;
+            }
+            
+            base.OnActionExecuting(context);
+        }
     }
 }
