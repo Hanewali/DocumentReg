2f9eb0432689b1c63dea7715ca0d52f4ceffe030
Jakub Rumpel <jakub@life-coder.pl>
2021-05-30 14:37:30 +0200

    Added basic actions to EmployeeController.cs

--- DocumentRegistry/DocumentRegistry.Web/Controllers/EmployeeController.cs
+++ DocumentRegistry/DocumentRegistry.Web/Controllers/EmployeeController.cs
@@ -1,13 +1,132 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using DocumentRegistry.Web.ApiModels;
+using DocumentRegistry.Web.Models.Employee;
+using DocumentRegistry.Web.Services.EmployeeService;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace DocumentRegistry.Web.Controllers
 {
-    public class EmployeeController : Controller
+    public class EmployeeController : BaseController
     {
-        // GET
-        public IActionResult Index()
+        private readonly ILogger<EmployeeController> _logger;
+        private static IEmployeeService _companyService;
+
+        public EmployeeController(IEmployeeService companyService, ILogger<EmployeeController> logger)
         {
-            return View();
+            _companyService = companyService;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Search(EmployeeRequest model)
+        {
+            var searchResult = new List<Employee>();
+            
+            try
+            {
+                searchResult.AddRange(_companyService.Search(model.Employee, model.BeginFrom, model.Rows, GetUserIdFromSession()));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "There was an error during company search");
+                return Problem();
+            }
+
+            return Ok(JsonSerializer.Serialize(searchResult));
+        }
+
+        [HttpGet]
+        public IActionResult Details(int companyId)
+        {
+            var result = new Employee();
+
+            try
+            {
+                result = _companyService.GetDetails(companyId, GetUserIdFromSession());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "There was an error during company search");
+                return Problem();
+            }
+
+            return View(result);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var model = new Employee();
+
+            return View(model);
+        }
+        
+        [HttpPost]
+        public IActionResult Create(Employee company)
+        {
+            try
+            {
+                _companyService.Create(company, GetUserIdFromSession());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "There was an error during company search");
+                return Problem();
+            }
+
+            return RedirectToAction("Search", "Employee");
+        }
+
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            var model = new Employee();
+
+            return View(model);
+        }
+        
+        [HttpPost]
+        public IActionResult Edit(Employee company)
+        {
+            try
+            {
+                _companyService.Edit(company, GetUserIdFromSession());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "There was an error during company search");
+                return Problem();
+            }
+
+            return RedirectToAction("Details", "Employee", company.Id);
+        }
+        
+        [HttpGet]
+        public IActionResult Delete()
+        {
+            var model = new Employee();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int companyId)
+        {
+            try
+            {
+                _companyService.Delete(companyId, GetUserIdFromSession());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "There was an error during company search");
+                return Problem();
+            }
+
+            return RedirectToAction("Search", "Employee");
         }
     }
 }
