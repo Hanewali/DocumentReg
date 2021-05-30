b0ac19ec0d2d2dd64a02c22ffb2f03bf514f0f57
Jakub Rumpel <jakub@life-coder.pl>
2021-05-30 15:13:26 +0200

    Added separate home controller with index action

---
+++ DocumentRegistry/DocumentRegistry.Web/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocumentRegistry.Web.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return HttpContext.Session.GetInt32("UserId") != null 
+                ? RedirectToAction("Search", "Letter") 
+                : RedirectToAction("Login", "Login");
+        }
+    }
+}
