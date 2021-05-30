dc37bdb7c9cd6dd244eb3e436ffd707cf9d2c68d
Jakub Rumpel <jakub@life-coder.pl>
2021-05-30 18:39:52 +0200

    Added remaining fields to Letter model

--- DocumentRegistry/DocumentRegistry.Web/Models/Letter/Letter.cs
+++ DocumentRegistry/DocumentRegistry.Web/Models/Letter/Letter.cs
@@ -6,13 +6,28 @@ namespace DocumentRegistry.Web.Models.Letter
     {
         public int Id { get; set; }
         public int Number { get; set; }
-        public int PostCompanyId { get; set; }
+        public PostCompany.PostCompany PostCompany { get; set; }
         public DateTime Date { get; set; }
         public DateTime ReceiveDate { get; set; }
         public string Content { get; set; }
-        public Employee.Employee EmployeeId { get; set; }
-        public Company.Company CompanyId { get; set; }
+        public Employee.Employee Employee { get; set; }
+        public Company.Company Company { get; set; }
         public DocumentType.DocumentType DocumentType { get; set; }
+        public string Other { get; set; }
+        public string PR { get; set; }
+        public string PO { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyStreet { get; set; }
+        public string CompanyCity { get; set; }
+        public string CompanyPostalCode { get; set; }
+        public string CompanyPostName { get; set; }
+        public string CompanyPostStreet { get; set; }
+        public string CompanyPostCity { get; set; }
+        public string CompanyPostPostalCode { get; set; }
+        public string EmployeeFirstName { get; set; }
+        public string EmployeeLastName { get; set; }
+        public string Kind { get; set; } //inbox, outbox
+        
         
     }
 }
