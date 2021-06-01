1e2d1ec792db375500f6ee37e932d934bca1ac99
Jakub Rumpel <jakub@life-coder.pl>
2021-06-01 08:52:48 +0200

    Added createdit model

---
+++ DocumentRegistry/DocumentRegistry.Web/Models/Employee/CreateEdit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DocumentRegistry.Web.Models.Employee
+{
+    public class CreateEdit
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<SelectListItem> Companies { get; set; }
+        public int SelectedCompanyId { get; set; }
+
+        public static CreateEdit FromDomainModel(Employee employee, IEnumerable<Company.Company> companies)
+        {
+            var model = new CreateEdit
+            {
+                FirstName = employee.FirstName,
+                LastName = employee.LastName,
+                Companies = companies.ToList().ConvertAll(x => new SelectListItem
+                {
+                    Text = x.Name, Value = x.Id.ToString(), Selected = false
+                }),
+                SelectedCompanyId = employee.Company.Id
+            };
+
+            model.Companies.First(x => x.Value == model.SelectedCompanyId.ToString()).Selected = true;
+
+            return model;
+        }
+    }
+}
