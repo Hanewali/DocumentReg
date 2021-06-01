28e6098afbe9a9ec130db241b16bb0ed95f26013
Jakub Rumpel <jakub@life-coder.pl>
2021-06-01 23:16:31 +0200

    added base columns to DocumenType

--- DocumentRegistry/DocumentRegistry.Database/dbo/Tables/DocumentType.sql
+++ DocumentRegistry/DocumentRegistry.Database/dbo/Tables/DocumentType.sql
@@ -1,5 +1,10 @@
 ﻿CREATE TABLE [dbo].[DocumentType] (
-    [Id]   INT            NOT NULL,
+    [Id]             INT            IDENTITY (1, 1) NOT NULL,
+    [CreateDate]     DATETIME       DEFAULT (getdate()) NOT NULL,
+    [CreateUserId]   INT            DEFAULT ((1)) NOT NULL,
+    [ModifyDate]     DATETIME       DEFAULT (getdate()) NOT NULL,
+    [ModifyUserId]   INT            DEFAULT ((1)) NOT NULL,
+    [IsActive]       BIT            DEFAULT ((1)) NOT NULL,
     [Name] NVARCHAR (150) NOT NULL,
     CONSTRAINT [PK_DocumentType] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
