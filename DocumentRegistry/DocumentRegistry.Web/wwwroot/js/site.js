3e14b4f7622264628f7a6c2fa941b6a064759b06
Jakub Rumpel <jakub@life-coder.pl>
2021-06-06 16:04:23 +0200

    Added styling of error messages

--- DocumentRegistry/DocumentRegistry.Web/wwwroot/js/site.js
+++ DocumentRegistry/DocumentRegistry.Web/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// $( function() {
+// } );
+
+var element = document.getElementsByClassName("field-validation-error")[0]
+if(element != undefined){
+    element.classList.add("alert")
+    element.classList.add("alert-danger")    
+}
+
