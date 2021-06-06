6b6093b7feeb44e78b20a8071e5dcfefe9d2e8a5
Jakub Rumpel <jakub@life-coder.pl>
2021-06-06 16:15:05 +0200

    Fixes and refactoring of company area

--- DocumentRegistry/DocumentRegistry.Web/Controllers/CompanyController.cs
+++ DocumentRegistry/DocumentRegistry.Web/Controllers/CompanyController.cs
@@ -26,6 +26,9 @@ namespace DocumentRegistry.Web.Controllers
         {
             var model = new Search();
 
+            if (TempData["Error"] != null)
+                ModelState.AddModelError("Error", TempData["Error"].ToString());
+
             try
             {
                 model.Companies = _companyService.Search(0, 10, GetUserIdFromSession());
@@ -43,29 +46,33 @@ namespace DocumentRegistry.Web.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Search(CompanyRequest model)
         {
-            var searchResult = new List<Company>();
+            var viewModel = new Search();
+            
+            if (TempData["Error"] != null)
+                ModelState.AddModelError("Error", TempData["Error"].ToString());
             
             try
             {
-                searchResult.AddRange(_companyService.Search(model.Company, model.BeginFrom, model.Rows, GetUserIdFromSession()));
+                viewModel.Companies = _companyService.Search(model.Company, model.BeginFrom, model.Rows, GetUserIdFromSession());
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "There was an error during company search");
-                return Problem();
+                _logger.LogError(ex, "There was an error during post company search");
+                ModelState.AddModelError("Error", "Wystąpił błąd podczas wyszukiwania firm pocztowych");
+                return View(viewModel);
             }
 
-            return Ok(JsonSerializer.Serialize(searchResult));
+            return View(viewModel);
         }
 
         [HttpGet]
-        public IActionResult Details([FromQuery] int companyId)
+        public IActionResult Details([FromQuery] int id)
         {
             var result = new Company();
 
             try
             {
-                result = _companyService.GetDetails(companyId, GetUserIdFromSession());
+                result = _companyService.GetDetails(id, GetUserIdFromSession());
             }
             catch (Exception ex)
             {
@@ -85,6 +92,7 @@ namespace DocumentRegistry.Web.Controllers
         }
         
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Create(Company company)
         {
             try
@@ -101,14 +109,15 @@ namespace DocumentRegistry.Web.Controllers
         }
 
         [HttpGet]
-        public IActionResult Edit(int companyId)
+        public IActionResult Edit(int id)
         {
-            var model = _companyService.GetDetails(companyId, GetUserIdFromSession());
+            var model = _companyService.GetDetails(id, GetUserIdFromSession());
 
             return View(model);
         }
         
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Edit(Company company)
         {
             try
@@ -121,7 +130,7 @@ namespace DocumentRegistry.Web.Controllers
                 return Problem();
             }
 
-            return RedirectToAction("Details", "Company", new {companyId = company.Id});
+            return RedirectToAction("Details", "Company", new {id = company.Id});
         }
         
         [HttpGet]
@@ -133,6 +142,7 @@ namespace DocumentRegistry.Web.Controllers
         }
         
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Delete(int Id)
         {
             try
