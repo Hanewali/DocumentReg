0c9063e130982c0765a933296e600f860debe3c8
Jakub Rumpel <jakub@life-coder.pl>
2021-06-20 00:25:28 +0200

    Dodanie kontroler ai modelu DocumentDirection

---
+++ DocumentRegistry/DocumentRegistry.Api/Controllers/DocumentDirectionController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
+using DocumentRegistry.Api.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace DocumentRegistry.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class DocumentDirectionController : BaseController
+    {
+        private readonly ILogger<DocumentDirectionController> _logger;
+
+        public DocumentDirectionController(ILogger<DocumentDirectionController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetList()
+        {
+            var result = new List<DomainModels.DocumentDirection>();
+
+            try
+            {
+                result.AddRange(DatabaseHelper.GetAll<DomainModels.DocumentDirection>());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error during DocumentDirection GetList");
+                return new StatusCodeResult((int) HttpStatusCode.InternalServerError);
+            }
+
+            return Ok(JsonSerializer.Serialize(result));
+        }
+    }
+}
---
+++ DocumentRegistry/DocumentRegistry.Api/DomainModels/DocumentDirection.cs
@@ -0,0 +1,8 @@
+﻿namespace DocumentRegistry.Api.DomainModels
+{
+    public class DocumentDirection
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
