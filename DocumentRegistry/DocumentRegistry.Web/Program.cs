09155447a413fc2243252152a558522e4570c0eb
Jakub Rumpel <jakub@rumpel.dev>
2023-03-11 23:34:35 +0100

    Changed startup config

--- DocumentRegistry/DocumentRegistry.Web/Program.cs
+++ DocumentRegistry/DocumentRegistry.Web/Program.cs
@@ -39,7 +39,7 @@ namespace DocumentRegistry.Web
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .UseContentRoot(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"/apps/DocumentRegistry/Web")
+                .UseContentRoot(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"/apps/DocReg.Web/")
                 .UseSerilog((context, services, configuration) => configuration
                     .ReadFrom.Configuration(context.Configuration)
                     .ReadFrom.Services(services)
